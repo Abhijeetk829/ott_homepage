c79d8ad537ebd77d9fa9f9250ce6e19cf18cfbed
Abhijeet Kumar <abhijeetk829@gmail.com>
2024-07-27 12:30:02 +0530

    added exception to run home only in non-homepage

--- src/home.js
+++ src/home.js
@@ -12,5 +12,7 @@ const a = document.createElement('a');
 a.href = "https://abhijeetk829.github.io/ott_homepage/";
 const homeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e8eaed"><path d="M230.77-190.77h161.54v-240h175.38v240h161.54v-373.85L480-753.46 230.77-564.87v374.1ZM200-160v-420l280-211.54L760-580v420H536.92v-240H423.08v240H200Zm280-312.23Z"/></svg>`;
 a.innerHTML = homeIcon;
-div.appendChild(a)
-document.body.appendChild(div);
+div.appendChild(a);
+if (window.location.href != "https://abhijeetk829.github.io/ott_homepage/") {
+    document.body.appendChild(div);
+}
